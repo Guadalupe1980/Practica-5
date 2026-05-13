068df7272b60a4200973f996af18590ad2862962
Jose <jj300254@gmail.com>
2026-05-13 09:59:07 -0600

    correccion de enlace en el index

---
+++ javascripts/ejercicioday13.js
@@ -0,0 +1,11 @@
+let UserRole ="admin"
+let UserName = "guadalupe"
+
+if (UserRole === "admin"){
+    alert("Bienvenido admin")
+    if(UserName === "guadalupe"){
+        alert("estas despedido")
+    }
+}else {
+    alert("Bienvenido colaborador")
+}
